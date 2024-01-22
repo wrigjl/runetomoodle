7ee96d8d11a85eda3b2d312f7c0faadfcb62ed12
Jason L. Wright <jason@thought.net>
2024-01-21 21:58:52 -0700

    fix up linter, add pltomoode converter

--- pltomoodle.py
+++ pltomoodle.py
@@ -1,14 +1,6 @@
 
 '''
-Convert a Runestone CSV file to something pleasing to Moodle XML
-
-One huge assumption here is that the assignments are named:
-  ^Chapter ([0-9]+) - .*$
-
-Also, each corresponding assignment in Moodle needs to have
-a unique number.  I assume it is Reading[0-9+] (in other words,
-I use the chapter title's number to generate the Moodle assignment
-number).
+Convert a PrairieLearn CSV file to something pleasing to Moodle
 '''
 
 import csv
@@ -18,16 +10,13 @@ import sqlite3
 import re
 from xml.dom import minidom
 
-db = sqlite3.connect('students.db')
-
 # Don't need these fields and having "E-mail" come first
 # makes this easier
-fields_to_delete = ['FName', 'LName', 'UName', 'Practice']
-
-# No grade markers
-nograde_markers = ['Not Started', 'Finished']
+fields_to_delete = ['UID', 'Name', 'Role', 'UIN']
 
 skip_users = set()
+
+db = sqlite3.connect('students.db')
 
 def load_skip_users(su):
     """When students drop, we want to skip them (less noise on import)"""
@@ -37,51 +26,35 @@ def load_skip_users(su):
             if len(line) > 0:
                 su.add(line)
 
-def translate_users(u):
-    """Some users use the wrong email: translate 'from' to 'to'"""
-    with open("translate.csv", encoding='UTF-8') as infile:
-        reader = csv.DictReader(infile)
-        for x in reader:
-            if u == x['from']:
-                return x['to']
-    return u
 
 def fix_row(row):
-    '''Fix up one Runestone gradebook row: delete uncessary fields,
-    strip away %'s, and mark empty those fields which have no grade
+    '''Fix up one PL gradebook row: ignore staff, and delete unnecessary fields.
     '''
-
-    if row['E-mail'] in skip_users:
+    if row['Role'] == 'Staff':
         return None
+
+    if row['UID'] in skip_users:
+        return None
+
+    zeros = []
+    for i in row.keys():
+        if i.startswith('HW') and len(row[i]) == 0:
+            zeros.append(i)
+    for i in zeros:
+        row[i] = '0'
+
+    cur = db.cursor()
+    cur.execute('select id from studentid where email = ?', (row['UID'], ))
+    rows = cur.fetchall()
+    assert len(rows) < 2
+    if len(rows) == 0:
+        print(f"ID unknown for {row['UID']}... skipping")
+        return None
+    row['id'] = rows[0][0]
 
     for name in fields_to_delete:
         if name in row:
             del row[name]
-
-    for field, value in row.items():
-        # Skip the email field
-        if field == 'E-mail':
-            continue
-
-        # Mark the nograde markers empty
-        if value in nograde_markers:
-            row[field] = ''
-            continue
-
-        # Get rid of trailing %'s in the grades
-        row[field] = value.rstrip('%')
-
-    row['E-mail'] = row['E-mail'].lower()
-    row['E-mail'] = translate_users(row['E-mail'])
-
-    cur = db.cursor()
-    cur.execute('select id from studentid where email = ?', (row['E-mail'], ))
-    rows = cur.fetchall()
-    assert len(rows) < 2
-    if len(rows) == 0:
-        print(f"ID unknown for {row['E-mail']}... skipping")
-        return None
-    row['id'] = rows[0][0]
 
     return row
 
@@ -100,7 +73,7 @@ def handle_row(docroot, results, row):
     for field, value in row.items():
         if field in ('E-mail', 'id'):
             continue
-        m = re.match(r'''^Chapter ([0-9]+) - .*$''', field)
+        m = re.match(r'''^HW([0-9]+)$''', field)
         if m is None:
             print(f"skipping field {field}:{value}")
             continue
@@ -116,7 +89,7 @@ def handle_row(docroot, results, row):
         results.appendChild(result)
 
         create_simple_text_node(docroot, result,
-                                'assignment', f"Reading{assignment}")
+                                'assignment', f"Quiz{assignment}")
 
         create_simple_text_node(docroot, result,
                                 'student', f"{row['id']}")
@@ -130,8 +103,8 @@ def main():
     load_skip_users(skip_users)
 
     parser = argparse.ArgumentParser(description='runetomoodle')
-    parser.add_argument('-i', '--input', help="input csvfile", default="input.csv")
-    parser.add_argument('-o', '--output', help="output xml file", default="output.xml")
+    parser.add_argument('-i', '--input', help="input csvfile", default="pl-input.csv")
+    parser.add_argument('-o', '--output', help="output xml file", default="pl-output.xml")
     args = parser.parse_args()
 
     with open(args.input, encoding='UTF-8') as infile, \
@@ -148,14 +121,9 @@ def main():
         root.appendChild(results)
 
         for row in reader:
-            if row['E-mail'] == '':
-                continue
-
             row = fix_row(row)
-            if row is None:
-                continue
-
-            handle_row(root, results, row)
+            if row:
+                handle_row(root, results, row)
 
         outfile.write(root.toprettyxml(indent='    '))
 
